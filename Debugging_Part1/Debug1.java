27800295e7040bde363df22dcdbc4586a9b0e641
ahrarhayat <ahrarhayat@gmail.com>
2019-02-23 02:21:08 +0600

    Initial commit

---
+++ Debugging_Part1/Debug1.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of Debug1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Debug1 {
+public void findAbc(String input) {
+    int index = input.indexOf("abc");
+    while (true) {
+        if (index == -1 || index >= input.length() - 3||index+4>=input.length()) {
+            System.out.println("Broken");
+            break;
+        }
+        String found = input.substring(index+1, index+4);
+        System.out.println(found);
+        index = input.indexOf("abc", index+4);
+        
+    }
+}
+   public void test() {
+    //no code yet
+    //String input= "abcdabc";
+    //findAbc( input);
+    
+    findAbc("yabcyabc");
+    findAbc("abcbbbabcdddabc");
+    
+    
+    
+    
+    
+}
+}
